4c0f5b393fb22f79291095a81c659ede41d447f1
Matthew Pickering <matthewtpickering@gmail.com>
2023-10-13 15:37:52 +0100

    ci: Introduce CROSS_STAGE variable
    In preparation for building and testing stage3 bindists we introduce the
    CROSS_STAGE variable which is used by a CI job to determine what kind of
    bindist the CI job should produce.
    
    At the moment we are only using CROSS_STAGE=2 but in the future we will
    have some jobs which set CROSS_STAGE=3 to produce native bindists for a
    target, but produced by a cross compiler, which can be tested on by
    another CI job on the native platform.
    
    CROSS_STAGE=2: Build a normal cross compiler bindist
    CROSS_STAGE=3: Build a stage 3 bindist, one which is a native compiler and library for the target

--- .gitlab/generate-ci/gen_ci.hs
+++ .gitlab/generate-ci/gen_ci.hs
@@ -148,6 +148,7 @@ data BuildConfig
                 , withNuma       :: Bool
                 , withZstd       :: Bool
                 , crossTarget    :: Maybe String
+                , crossStage     :: Maybe Int
                 , crossEmulator  :: CrossEmulator
                 , configureWrapper :: Maybe String
                 , fullyStatic    :: Bool
@@ -210,6 +211,7 @@ vanilla = BuildConfig
   , withNuma = False
   , withZstd = False
   , crossTarget = Nothing
+  , crossStage  = Nothing
   , crossEmulator = NoEmulator
   , configureWrapper = Nothing
   , fullyStatic = False
@@ -258,6 +260,7 @@ crossConfig :: String       -- ^ target triple
             -> BuildConfig
 crossConfig triple emulator configure_wrapper =
     vanilla { crossTarget = Just triple
+            , crossStage  = Just 2
             , crossEmulator = emulator
             , configureWrapper = configure_wrapper
             }
@@ -773,6 +776,7 @@ job arch opsys buildConfig = NamedJob { name = jobName, jobInfo = Job {..} }
       , "INSTALL_CONFIGURE_ARGS" =: "--enable-strict-ghc-toolchain-check"
       , maybe mempty ("CONFIGURE_WRAPPER" =:) (configureWrapper buildConfig)
       , maybe mempty ("CROSS_TARGET" =:) (crossTarget buildConfig)
+      , maybe mempty (("CROSS_STAGE" =:) . show) (crossStage buildConfig)
       , case crossEmulator buildConfig of
           NoEmulator       -> case crossTarget buildConfig of
             Nothing -> mempty
